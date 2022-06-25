d2d14d942522446b06723d02ea0c584831c1d8a7
Efsane2323 <106430183+Efsane2323@users.noreply.github.com>
2022-06-25 12:55:38 +0300

    Update start.py

--- kelime_bot/plugins/start.py
+++ kelime_bot/plugins/start.py
@@ -11,11 +11,11 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 keyboard = InlineKeyboardMarkup([
     [
-        InlineKeyboardButton("➕ Grubuna Ekle", url=f"http://t.me/KelimeHazinesiBot?startgroup=new")
+        InlineKeyboardButton("➕ Grubuna Ekle", url=f"http://t.me/kelime_evrenibot?startgroup=new")
     ],
     [
-        InlineKeyboardButton("🇹🇷 Sahibim", url="https://t.me/nevarevladim"),
-        InlineKeyboardButton("💬 Chat", url="https://t.me/+0kf_3zSzsCszZWJk"),
+        InlineKeyboardButton("🇹🇷 Sahibim", url="https://t.me/evrendestek"),
+        InlineKeyboardButton("💬 Chat", url="https://t.me/evrendestek"),
     ]
 ])
 
